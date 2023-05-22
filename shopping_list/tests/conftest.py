ca56e8ecbaa2f97cceb190fa58026023b00a17c5
MAru <maruma.morandini@gmail.com>
2023-05-21 23:22:40 -0300

    unitests

---
+++ shopping_list/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+
+from shopping_list.models import ShoppingItem, ShoppingList
+
+
+@pytest.fixture(scope="session")
+def create_shopping_item():
+    def __create_shopping_item(name):
+        shopping_list = ShoppingList.objects.create(name="My shopping list")
+        shopping_item = ShoppingItem.objects.create(name=name, purchased=False, shopping_list=shopping_list)
+        return shopping_item
+    return __create_shopping_item
--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -3,8 +3,8 @@ from shopping_list.api.views import ListAddShoppingList, ShoppingListDetail, Add
 
 
 urlpatterns = [
-    path("api/shopping-lists/", ListAddShoppingList.as_view(), name="all_shopping_lists"),
-    path("api/shopping-lists/<uuid:pk>", ShoppingListDetail.as_view(), name="shopping_list_detail"),
-    path("api/shopping-lists/<uuid:pk>/shopping-items/", AddShoppingItem.as_view(), name="add_shopping_item"),
-    path("api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>", ShoppingItemDetail.as_view(), name="shopping_item_detail"),
+    path("api/shopping-lists/", ListAddShoppingList.as_view(), name="all-shopping-lists"),
+    path("api/shopping-lists/<uuid:pk>", ShoppingListDetail.as_view(), name="shopping-list-detail"),
+    path("api/shopping-lists/<uuid:pk>/shopping-items/", AddShoppingItem.as_view(), name="add-shopping-item"),
+    path("api/shopping-lists/<uuid:pk>/shopping-items/<uuid:item_pk>", ShoppingItemDetail.as_view(), name="shopping-item-detail"),
 ]
